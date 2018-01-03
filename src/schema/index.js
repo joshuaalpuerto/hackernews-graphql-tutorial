450344e35634d1ba90c6edee47209c8dde591622
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-01-03 15:42:39 +0800

    Implemented a GUI for querying graphql

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,6 +2,7 @@
 const {
   makeExecutableSchema
 } = require('graphql-tools');
+const resolvers = require('./resolvers');
 
 // Define your types here.
 const typeDefs = `
@@ -10,7 +11,11 @@ const typeDefs = `
     url: String!
     description: String!
   }
+
+  type Query {
+    allLinks: [Link!]!
+  }
 `;
 
 // Generate the schema object from your types definition.
-module.exports = makeExecutableSchema({ typeDefs });
+module.exports = makeExecutableSchema({ typeDefs, resolvers });
